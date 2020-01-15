3f4d03fe297de469123a8a370f696d3444d0fbc4
DoYoung An <doyoung@toysmyth.com>
2020-01-15 15:31:16 +0900

    Create half donut chart lib file

---
+++ lib/halfDonutchart.ts
@@ -0,0 +1,15 @@
+import * as d3 from 'd3';
+
+export interface IHalfDonutchartConfig {
+  element: HTMLElement
+}
+
+const HalfDonutchart = (config: IHalfDonutchartConfig) => {
+
+  const {
+    element
+  } = config;
+
+};
+
+export default HalfDonutchart;
--- lib/index.ts
+++ lib/index.ts
@@ -4,9 +4,12 @@ import Timeline from './timeline';
 export { ITimelineConfig } from './timeline';
 import Piechart from './piechart';
 export { IPiechartConfig } from './piechart';
+import HalfDonutchart from './halfDonutchart';
+export { IHalfDonutchartConfig } from './halfDonutchart';
 
 export default {
   Linechart,
   Timeline,
-  Piechart
+  Piechart,
+  HalfDonutchart
 }
