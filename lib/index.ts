620085f2d4db5e7c9f883f54f46d859285c320f4
DoYoung An <doyoung@toysmyth.com>
2020-01-15 10:59:56 +0900

    Create piechart lib

--- lib/index.ts
+++ lib/index.ts
@@ -2,8 +2,11 @@ import Linechart from './linechart';
 export { ILinechartConfig } from './linechart';
 import Timeline from './timeline';
 export { ITimelineConfig } from './timeline';
+import Piechart from './piechart';
+export { IPiechartConfig } from './piechart';
 
 export default {
   Linechart,
-  Timeline
+  Timeline,
+  Piechart
 }
---
+++ lib/piechart.ts
@@ -0,0 +1,15 @@
+import * as d3 from 'd3';
+
+export interface IPiechartConfig {
+  element: HTMLElement
+}
+
+const Piechart = (config: IPiechartConfig) => {
+
+  const {
+    element
+  } = config;
+
+};
+
+export default Piechart;
