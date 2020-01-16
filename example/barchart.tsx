2225d736c0ea85c7c307d5abea9e0700a1effa03
DoYoung An <doyoung@toysmyth.com>
2020-01-16 11:14:24 +0900

    Create example bar chart component

---
+++ example/barchart.tsx
@@ -0,0 +1,17 @@
+import React, { FC, useRef, useEffect } from 'react';
+
+interface IBarchartProps {}
+
+const Barchart: FC<IBarchartProps> = (): JSX.Element => {
+
+  return (
+    <>
+      <div className="card" style={{ width: '33%', height: 400 }}>
+        
+      </div>
+    </>
+  );
+
+}
+
+export default Barchart;
