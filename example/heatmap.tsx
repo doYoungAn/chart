359b161f546495fb506c59f0f8acdb7a311cc60e
doYoungAn <jispoop@gmail.com>
2020-01-18 14:56:12 +0900

    Add heatmap config properties

--- example/heatmap.tsx
+++ example/heatmap.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useRef, useEffect } from 'react';
+import d3Extend, { IHeatmapConfig } from './../lib';
 
 interface IHeatmapProps {}
 
@@ -7,7 +8,57 @@ const Heatmap: FC<IHeatmapProps> = (): JSX.Element => {
     const wrapper = useRef(null);
 
     useEffect(() => {
-
+        const config: IHeatmapConfig = {
+            element: wrapper.current,
+            xRange: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+            yRange: ['1', '2', '3', '4', '5'],
+            valueRange: [0, 100],
+            minColor: '#ffffff',
+            maxColor: '#009688',
+            datas: [
+                { x: 'a', y: '1', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'a', y: '2', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'a', y: '3', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'a', y: '4', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'a', y: '5', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'b', y: '1', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'b', y: '2', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'b', y: '3', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'b', y: '4', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'b', y: '5', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'c', y: '1', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'c', y: '2', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'c', y: '3', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'c', y: '4', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'c', y: '5', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'd', y: '1', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'd', y: '2', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'd', y: '3', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'd', y: '4', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'd', y: '5', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'e', y: '1', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'e', y: '2', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'e', y: '3', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'e', y: '4', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'e', y: '5', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'f', y: '1', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'f', y: '2', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'f', y: '3', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'f', y: '4', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'f', y: '5', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'g', y: '1', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'g', y: '2', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'g', y: '3', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'g', y: '4', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'g', y: '5', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'h', y: '1', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'h', y: '2', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'h', y: '3', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'h', y: '4', value: Math.floor(Math.random() * 10) * 10 },
+                { x: 'h', y: '5', value: Math.floor(Math.random() * 10) * 10 },
+            ]
+        };
+        d3Extend.Heatmap(config);
     }, []);
 
     return (
--- lib/heatmap.ts
+++ lib/heatmap.ts
@@ -7,10 +7,23 @@ interface IChartMargin {
     left: number;
 }
 
+interface IHeatmapData {
+    x: string;
+    y: string;
+    value: number;
+}
+
 export interface IHeatmapConfig {
     element: HTMLElement;
-
+    xRange: string[];
+    yRange: string[];
+    valueRange: [number, number];
+    datas: IHeatmapData[];
     margin?: IChartMargin;
+    minColor?: string;
+    maxColor?: string;
+    xPadding?: number;
+    yPadding?: number;
 }
 
 const Margin = {
@@ -24,8 +37,15 @@ const Heatmap = (config: IHeatmapConfig) => {
 
     const {
         element,
-
-        margin = Margin
+        xRange,
+        yRange,
+        valueRange,
+        datas,
+        margin = Margin,
+        minColor = '#ffffff',
+        maxColor = '#00ACC1',
+        xPadding = 0.1,
+        yPadding = 0.1
     } = config;
 
     const width: number = element.clientWidth - margin.left - margin.right;
@@ -39,6 +59,46 @@ const Heatmap = (config: IHeatmapConfig) => {
     const parentGEl = svg
         .append('g')
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    const x = d3.scaleBand()
+        .domain(xRange)
+        .range([0, width])
+        .padding(xPadding);
+
+    parentGEl.append('g')
+        .attr('stroke-width', 2)
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(x));
+
+    const y = d3.scaleBand()
+        .domain(yRange)
+        .range([height, 0])
+        .padding(yPadding);
+
+    parentGEl.append('g')
+        .attr('stroke-width', 2)
+        .call(d3.axisLeft(y));
+
+    const colors = d3.scaleLinear()
+        .range([minColor, maxColor] as any)
+        .domain(valueRange)
+
+    const dataEls = parentGEl
+        .selectAll()
+        .data(datas)
+        .enter()
+        .append('rect')
+        .attr('x', (d) => x(d.x))
+        .attr('y', (d) => y(d.y))
+        
+        .attr('width', x.bandwidth())
+        .attr('height', y.bandwidth())
+        .style('fill', minColor)
+
+    dataEls
+        .transition()
+        .duration(1000)
+        .style('fill', (d) => colors(d.value))
 
 }
 
