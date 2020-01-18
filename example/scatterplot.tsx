495f8dbe5e7fc5cffb398b4bee8ceec0968bf162
doYoungAn <jispoop@gmail.com>
2020-01-18 15:41:32 +0900

    Complete scatterplot logic

--- example/scatterplot.tsx
+++ example/scatterplot.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useRef, useEffect } from 'react';
+import d3Extend, { IScatterplotConfig } from './../lib';
 
 interface IScatterplotProps {}
 
@@ -7,14 +8,59 @@ const Scatterplot: FC<IScatterplotProps> = (): JSX.Element => {
     const wrapper = useRef(null);
 
     useEffect(() => {
-
+        const config: IScatterplotConfig = {
+            element: wrapper.current,
+            datas: [
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+                { x: Math.floor(Math.random() * 10) * 10, y: Math.floor(Math.random() * 10) * 10 },
+            ]
+        };
+        d3Extend.Scatterplot(config);
     }, []);
 
     return (
         <>
             <div className="card" style={{ width: '33%', height: 400 }}>
                 <div className="card-title">Scatterplot</div>
-                <div ref={wrapper} style={{ width: '100%', height: '100%' }}></div>
+                <div ref={wrapper} style={{ width: '100%', height: 350 }}></div>
             </div>
         </>
     );
--- lib/scatterplot.ts
+++ lib/scatterplot.ts
@@ -7,10 +7,17 @@ interface IChartMargin {
     left: number;
 }
 
+interface IScatterplotData {
+    x: number;
+    y: number;
+}
+
 export interface IScatterplotConfig {
     element: HTMLElement;
-
+    datas: IScatterplotData[],
     margin?: IChartMargin;
+    plotColor?: string;
+    plotSize?: number;
 }
 
 const Margin = {
@@ -24,8 +31,10 @@ const Scatterplot = (config: IScatterplotConfig) => {
 
     const {
         element,
-
-        margin = Margin
+        datas,
+        margin = Margin,
+        plotColor = '#039BE5',
+        plotSize = 2
     } = config;
 
     const width: number = element.clientWidth - margin.left - margin.right;
@@ -39,6 +48,34 @@ const Scatterplot = (config: IScatterplotConfig) => {
     const parentGEl = svg.append('g')
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
     
+
+    const x = d3.scaleLinear()
+        .domain([0, 100])
+        .range([0, width]);
+
+    parentGEl.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .attr('stroke-width', 2)
+        .call(d3.axisBottom(x));
+
+    const y = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height, 0]);
+
+    parentGEl.append('g')
+        .attr('stroke-width', 2)
+        .call(d3.axisLeft(y))
+
+    parentGEl.append('g')
+        .selectAll('dot')
+        .data(datas)
+        .enter()
+        .append('circle')
+        .attr('cx', (d) => x(d.x))
+        .attr('cy', (d) => y(d.y))
+        .attr('r', plotSize)
+        .style('fill', plotColor)
+
 };
 
 export default Scatterplot;
